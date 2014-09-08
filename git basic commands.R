79173304a98fd6f9b2e26af93ab2c364671fc606
Ben <mpalanco@gmail.com>
2014-09-08 22:03:15 +0200

    "new"

---
+++ git basic commands.R
@@ -0,0 +1,30 @@
+git basic commands - Benjamin
+
+pwd diplays the path to the current working directory
+
+ls list files and folders in the current directory
+ls -a lists hidden and unhidden files and folders
+ls -al lists details for hidden and unhidden files and folders
+
+preceded by -  <- flagged
+
+http://stackoverflow.com/questions/19742256/path-doesnt-work-with-20-for-space-in-filename-in-git-console
+we can also use tab completion in case you have only one file with name starting with that string 
+
+cd change directory
+cd with no arguments takes you to your home directory
+cd .. allows you to change directory to one level above your current directory
+cd - get back
+
+mkdir makes directory
+touch creates empty file
+cp stands for copy. takes as its first argument a file, and as its second argument the path to where you want the file to be copied
+rm remove
+rm to delete entire directories and their contents by using the -r flag
+rm -r Mydocumentes 
+
+mv stands for move
+
+echo will print whatever arguments you provide
+
+date will print today's data
